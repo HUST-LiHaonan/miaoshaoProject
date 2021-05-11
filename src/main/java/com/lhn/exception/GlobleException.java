3c6be3ab885ced139aab49766fdca656f852a945
li <spikeli1998@gmail.com>
2021-05-11 11:15:48 +0800

    添加全局异常

---
+++ src/main/java/com/lhn/exception/GlobleException.java
@@ -0,0 +1,23 @@
+/**
+ * Copyright (C), 2016-2021, 华中科技大学
+ * FileName: GlobleException
+ * Author:   li
+ * Date:     2021/5/11 上午11:15
+ * Description:
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.lhn.exception;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈〉
+ *
+ * @author li
+ * @create 2021/5/11
+ * @since 1.0.0
+ */
+public class GlobleException extends RuntimeException{
+    
+}
